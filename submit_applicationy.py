99d7ab665924303eb9c009128f17926509139b79
Harrison <li.harrison.h@gmail.com>
2026-03-15 23:59:44 -0700

    Create submit_applicationy.py

---
+++ submit_applicationy.py
@@ -0,0 +1,31 @@
+import hashlib, hmac, json, requests
+from urllib.parse import urlencode
+
+secret = "hg2026_python_engineer@!"
+endpoint = "https://howgood-apply-api.howgood.workers.dev/apply"
+
+resume_link: str = urlencode()
+payload = {
+    "name": "Harrison Li",
+    "email": "Li.Harrison.h@gmail.com",
+    "resume": "https://github.com/hhprogram/Resume/blob/054de727811118d807e10f47c270803a65f005d7/Harrison%20-%20Resume%20(2026).pdf",       # URL to your resume
+    "location": "Berkeley, CA",     # e.g. "New York, NY"
+    "linkedin": "linkedin.com/in/liharrisonh",
+    "codeLink": "",     # URL to the repo/gist containing THIS script
+    "yearsPython": 7,
+    "yearsDjango": 2,
+    "notes": "Found HowGood on climatebase.org as I search for opportunities leveraging software engineering to nudge society onto a better path."
+}
+
+body = json.dumps(payload)
+signature = hmac.new(secret.encode(), body.encode(), hashlib.sha256).hexdigest()
+
+resp = requests.post(
+    endpoint,
+    data=body,
+    headers={
+        "Content-Type": "application/json",
+        "X-HMAC-Signature": signature,
+    },
+)
+print(resp.status_code, resp.json())
